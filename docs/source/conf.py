f3f55e528fc882beffce788f47f2e40aa28809c0
bryansx <46893530+bryansx@users.noreply.github.com>
2023-02-20 18:20:22 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
 
-import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.append("../..")  # Adds higher directory to python modules path.
 from novalabs.utils import *
 
 # -- Project information
